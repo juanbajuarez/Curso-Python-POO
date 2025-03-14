d385913170ae9b167b18d2401a384f2ce17801ca
SALADECOMPUTO <juanb8935@gmail.com>
2025-03-13 22:01:42 -0600

    SC 02 POO.

--- Clases-y-Objetos/Ej1_Personaje.py
+++ Clases-y-Objetos/Ej1_Personaje.py
@@ -12,31 +12,38 @@ class Personaje:
         Personaje.contador_id+=1
 
     def moverse(self,ordenes:str)->None:
-        movimientos=len(ordenes)
-        for i in range (0,movimientos-1):
+        for i in range(0,len(ordenes)):
             if ordenes[i]=='A' or ordenes[i]=='a':
-                if 0<self.posy <10:
+                if 0<=self.posy <10:
                     self.posy+=1
             elif ordenes[i]=='R' or ordenes[i]=='r':
                 if self.posy!=0:
                     self.posy-=1
-
             elif ordenes[i]=='D' or ordenes[i]=='d':
-                if 0 < self.posx < 10:
+                if 0 <=self.posx < 10:
                     self.posx += 1
             elif ordenes[i]=='I' or ordenes[i]=="i":
-                if self.posy != 0:
-                    self.posy -= 1
+                if self.posx != 0:
+                    self.posx -= 1
             else:
                 print("Datos incorrectos")
 
     def posicion_actual(self)->None:
-        pass
+        print(f"Posicion actual: (x,y)= {self.posx,self.posy}")
     def __str__(self) -> str:
-        return f"Posicion actual: (x,y)= {self.posicion_actual()})"
+        return f"Personaje(id:{self.id_Personaje},(x,y)= {self.posx,self.posy})"
 
 if __name__ == '__main__':
     personaje1=Personaje()
     print(personaje1)
-    instruccion=None
-    while instruccion!='s' or instruccion!='S'
+    personaje2 = Personaje()
+    print(personaje2)
+    ban=None
+    while ban!=1:
+        mov=input("Ingrese las ordenes de movimiento: ")
+        if mov=='s' or mov=='S':
+            print("Programa terminado")
+            break
+        else:
+            personaje1.moverse(mov)
+            personaje1.posicion_actual()
